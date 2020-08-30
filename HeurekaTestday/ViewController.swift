63f842cda588376ab718633d0bcbd7bf8d440321
Jan Seidl <honza.seidl@gmail.com>
2020-08-30 15:38:00 +0200

    Initial Commit

---
+++ HeurekaTestday/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HeurekaTestday
+//
+//  Created by JSE on 30/08/2020.
+//  Copyright © 2020 Hany Co. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
